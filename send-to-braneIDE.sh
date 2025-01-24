95863daafe19f68c5fb7c57761ce3dc9fe2e8167
Benny R <Benny.Rigler@os3.nl>
2025-01-24 11:50:53 +0000

    clean up

--- send-to-braneIDE.sh
+++
@@ -1,2 +0,0 @@
-sftp benny@145.100.106.153:/home/benny/brane-ide_fix/certs/ <<< $'put client_cert/ca.pem'
-sftp benny@145.100.106.153:/home/benny/brane-ide_fix/certs/ <<< $'put client_cert/client-id.pem'
